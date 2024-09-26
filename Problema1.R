80b888a5e6422e647d047262c99cf1b65bf50c43
Your Name <you@example.com>
2024-09-26 11:28:53 +0200

    s

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,15 @@
 #Codigo para problema 1
 
+#coeficient de determinacio -> multiple r-squared
+rsq = 0.1923
+
+#coeficient de correlació -> sqrt(rsq)
+0.4385202
+
+#variació del valor augmentant 1 unitat -> primer llistat; assets(valor estimat)
+0.06576
+
+#valor predecido por activos = 647.1 -> 0.06576*647.1 + 11.79755
+54.35085
+
+
